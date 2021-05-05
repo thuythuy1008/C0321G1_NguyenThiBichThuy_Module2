7fa2e9170cf57f1bd881a7ef5815f75f6b1a58e6
unknown <thuythuy100897@gmail.com>
2021-05-05 17:19:39 +0700

    commit bai tap bai 4

---
+++ src/bai4_lop_va_doi_tuong_trong_java/bai_tap/bai_tap_3_Fan/FanMain.java
@@ -0,0 +1,12 @@
+package bai4_lop_va_doi_tuong_trong_java.bai_tap.bai_tap_3_Fan;
+
+public class FanMain {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3, true, 10, "Yellow");
+        Fan fan2 = new Fan(2, false, 5, "Blue");
+//        fan1.toString();
+//        fan2.toString();
+        System.out.println(fan1);
+        System.out.println(fan2);
+    }
+}
