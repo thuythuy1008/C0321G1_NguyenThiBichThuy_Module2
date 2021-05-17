80afe9a3360e32d789613a02c3cb95ce7476107a
unknown <thuythuy100897@gmail.com>
2021-05-17 21:17:54 +0700

    commit bai tap bai 13

--- src/bai13_thuat_toan_sap_xep/thuc_hanh/thuc_hanh_3_Cai_Dat_Thuat_Toan_Sap_Xep_Chon/SelectionSort.java
+++ src/bai13_thuat_toan_sap_xep/thuc_hanh/thuc_hanh_3_Cai_Dat_Thuat_Toan_Sap_Xep_Chon/SelectionSort.java
@@ -5,7 +5,6 @@ public class SelectionSort {
 
     public static void selectionSort(double[] list) {
         for (int i = 0; i < list.length - 1; i++) {
-            /* Find the minimum in the list[i..list.length-1] */
             double currentMin = list[i];
             int currentMinIndex = i;
 
@@ -16,7 +15,6 @@ public class SelectionSort {
                 }
             }
 
-            /* Swap list[i] with list[currentMinIndex] if necessary */
             if (currentMinIndex != i) {
                 list[currentMinIndex] = list[i];
                 list[i] = currentMin;
