9210954e9de5d809d506fcb53de46043ca32ced6
unknown <thuythuy100897@gmail.com>
2021-05-24 09:01:08 +0700

    commit bai tap bai 15

--- src/bai15_io_text_file/bai_tap/bai_tap_1_Copy_File_Text/CopyFile.java
+++ src/bai15_io_text_file/bai_tap/bai_tap_1_Copy_File_Text/CopyFile.java
@@ -3,33 +3,26 @@ package bai15_io_text_file.bai_tap.bai_tap_1_Copy_File_Text;
 import java.io.*;
 
 public class CopyFile {
-    public static void copyFile(String filePath) {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
         try {
-            File file = new File(filePath);
+            inputStream = new FileInputStream(new File("src\\bai15_io_text_file\\bai_tap\\bai_tap_1_Copy_File_Text\\source.txt"));
+            outputStream = new FileOutputStream(new File("src\\bai15_io_text_file\\bai_tap\\bai_tap_1_Copy_File_Text\\target.txt"));
 
-            if (!file.exists()) {
-                throw new FileNotFoundException();
-            }
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = "";
-            FileWriter fileWriter = new FileWriter("src\\bai15_io_text_file\\bai_tap\\bai_tap_1_Copy_File_Text\\target.txt", false);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            int length;
+            byte[] temp = new byte[1024];
 
-            while ((line = bufferedReader.readLine()) != null) {
-                bufferedWriter.write(line + "\n");
+            while ((length = inputStream.read(temp)) > 0) {
+                outputStream.write(temp, 0, length);
             }
-            bufferedReader.close();
-            bufferedWriter.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("Fie không tồn tại!!!");
+            System.out.println("File đã được sao chép thành công!!!");
         } catch (IOException e) {
-            System.err.println("Nội dung có lỗi!!!");
+            e.printStackTrace();
+        } finally {
+            inputStream.close();
+            outputStream.close();
         }
-    }
-
-    public static void main(String[] args) {
-        String filePath = "src\\bai15_io_text_file\\bai_tap\\bai_tap_1_Copy_File_Text\\source.txt";
-        copyFile(filePath);
     }
 }
