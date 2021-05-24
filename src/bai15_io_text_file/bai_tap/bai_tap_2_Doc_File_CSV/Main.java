2c61b762f6a6457ec982afbe80ba8ae7dc0be80d
unknown <thuythuy100897@gmail.com>
2021-05-24 09:36:39 +0700

    commit bai tap bai 15

---
+++ src/bai15_io_text_file/bai_tap/bai_tap_2_Doc_File_CSV/Main.java
@@ -0,0 +1,50 @@
+package bai15_io_text_file.bai_tap.bai_tap_2_Doc_File_CSV;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        try {
+            String line;
+            br = new BufferedReader(new FileReader("src\\bai15_io_text_file\\bai_tap\\bai_tap_2_Doc_File_CSV\\countries.csv"));
+
+            while ((line = br.readLine()) != null) {
+                printCountry(parseCsvLine(line));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static List<String> parseCsvLine(String csvLine) {
+        List<String> result = new ArrayList<>();
+        if (csvLine != null) {
+            String[] splitData = csvLine.split(",");
+            for (int i = 0; i < splitData.length; i++) {
+                result.add(splitData[i]);
+            }
+        }
+        return result;
+    }
+
+    private static void printCountry(List<String> country) {
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + " , name=" + country.get(2)
+                        + "]");
+    }
+}
