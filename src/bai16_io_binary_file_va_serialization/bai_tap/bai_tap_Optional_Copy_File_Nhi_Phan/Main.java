031bac772506b34775da6dd637d37d883699fc91
unknown <thuythuy100897@gmail.com>
2021-05-25 10:50:21 +0700

    commit bai tap bai 16

--- src/bai16_io_binary_file_va_serialization/bai_tap/bai_tap_Optional_Copy_File_Nhi_Phan/Main.java
+++ src/bai16_io_binary_file_va_serialization/bai_tap/bai_tap_Optional_Copy_File_Nhi_Phan/Main.java
@@ -1,6 +1,7 @@
 package bai16_io_binary_file_va_serialization.bai_tap.bai_tap_Optional_Copy_File_Nhi_Phan;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void copyFile(File source, File target) throws FileNotFoundException, IOException {
@@ -23,13 +24,27 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         try {
-            String source = ("src\\bai16_io_binary_file_va_serialization\\bai_tap\\bai_tap_Optional_Copy_File_Nhi_Phan\\source.csv");
-            String target = ("src\\bai16_io_binary_file_va_serialization\\bai_tap\\bai_tap_Optional_Copy_File_Nhi_Phan\\target.csv");
+            String source = ("src\\bai16_io_binary_file_va_serialization\\bai_tap\\bai_tap_Optional_Copy_File_Nhi_Phan\\source.jpg");
+            String target = ("src\\bai16_io_binary_file_va_serialization\\bai_tap\\bai_tap_Optional_Copy_File_Nhi_Phan\\target.jpg");
             File sourceFile = new File(source);
             File targetFile = new File(target);
+            if (targetFile.exists()) {
+                System.err.println("File đã tồn tại! Bạn có muốn ghi đè file không? \n" +
+                        "1. Có \n" +
+                        "2. Không \n");
+                Scanner scanner = new Scanner(System.in);
+                String choose = scanner.nextLine();
+                switch (choose) {
+                    case "1":
+                        copyFile(sourceFile, targetFile);
+                        System.exit(1);
+                    case "2":
+                        System.exit(1);
+                }
+            }
             copyFile(sourceFile, targetFile);
         } catch (FileNotFoundException e) {
-            System.out.println("Ecxeption occured:" + e);
+            System.err.println("Ecxeption occured:" + e);
             System.out.println("Vui lòng chọn file!!!");
         } catch (IOException e) {
             e.printStackTrace();
